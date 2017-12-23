4e94afd864560470ceb8448041628115822a2bc8
Medi W. Assumani <mediassumani49@gmail.com>
2017-12-22 22:47:12 -0500

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+#include "implementation.cpp"
+
+using namespace std;
+
+int main(){
+
+
+    Animal obj1("simba","mammal","Tundra","Carnivore",false,12);
+    Animal obj2(obj1); // ussing the copy constructor
+
+
+    obj1.printAnimalInfo();
+    obj2.printAnimalInfo();
+
+
+
+    return 0;
+}
 
